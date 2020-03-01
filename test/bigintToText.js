3ad90623dd5ec0c4408b9de02b9b08b0d9cbc5ed
juanelas <j.hernandez@upc.edu>
2020-03-01 19:13:03 +0100

    Some new tests

---
+++ test/bigintToText.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// For the browser test builder to work you MUST import the module in a variable that
+// is the camelised version of the package name.
+const bigintConversion = require('../dist/bigint-conversion-latest.node');
+const chai = require('chai');
+
+const inputs = [
+    "Hello World",
+    "Apañarse por qué?",
+    `Lorem ipsum dolor sit amet, consectetur adipisci tempor incidunt ut labore et dolore magna aliqua veniam, quis nostrud exercitation ullamcorpor s commodo consequat. Duis autem vel eum irrure esse molestiae consequat, vel illum dolore eu fugi et iusto odio dignissim qui blandit praesent luptat exceptur sint occaecat cupiditat non provident, deserunt mollit anim id est laborum et dolor fuga distinct. Nam liber tempor cum soluta nobis elige quod maxim placeat facer possim omnis volupt
+    
+    Lorem ipsum dolor si amet, consectetur adipiscing incidunt ut labore et dolore magna aliquam erat nostrud exercitation ullamcorper suscipit laboris nis duis autem vel eum irure dolor in reprehenderit i, dolore eu fugiat nulla pariatur. At vero eos et accusa praesant luptatum delenit aigue duos dolor et mole provident, simil tempor sunt in culpa qui officia de fuga. Et harumd dereud facilis est er expedit disti eligend optio congue nihil impedit doming id quod assumenda est, omnis dolor repellend. Temporibud
+    
+    Lorem ipsum dolor si amet, consectetur adipiscing incidunt ut labore et dolore magna aliquam erat nostrud exercitation ullamcorper suscipit laboris nis duis autem vel eum irure dolor in reprehenderit i dolore eu fugiat nulla pariatur. At vero eos et accus praesant luptatum delenit aigue duos dolor et mol provident, simil tempor sunt in culpa qui officia de fuga. Et harumd dereud facilis est er expedit disti eligend oprio congue nihil impedit doming id quod assumenda est, omnis dolor repellend. Temporibud`
+];
+
+describe('bigintToText((textToBigint(str))) === str ', function () {
+    for (const input of inputs) {
+        describe(`bigintToText((textToBigint(${input})))`, function () {
+            it(`should return ${input}`, function () {
+                const ret = bigintConversion.bigintToText(bigintConversion.textToBigint(input));
+                chai.expect(ret).to.equal(input);
+            });
+        });
+    }
+});
